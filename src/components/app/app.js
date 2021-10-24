cc036ed1c15064abee1a71f4d13855e550d5a611
vakulanikita <nik.vakula@bk.ru>
2021-10-24 11:04:11 +0300

    adding a todo component

---
+++ src/components/app/app.js
@@ -0,0 +1,29 @@
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from '../item-status-filter';
+
+import './app.css';
+
+const App = () => {
+  // этот массив должен подтягиваться с сервера со своими key ключами
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a great day', important: false, id: 3 },
+  ]
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+}
+
+export default App;
