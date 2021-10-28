2ee37bf6c53ef580fdae0c51ba0577dea0d8e4e1
vakulanikita <nik.vakula@bk.ru>
2021-10-28 18:19:25 +0300

    delete, important buttons + strikethrough an element

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,40 +7,63 @@ import TodoList from '../todo/todo-list';
 import ItemStatusFilter from '../todo/item-status-filter';
 
 import './app.css';
+import { Component } from 'react';
 
-const App = () => {
+export default class App extends Component  {
 
-  const tableData = [
-    {name: 'Nikita Vakula', days: 6, salary: 30, id: 1},
-    {name: 'Danil Zakharov', days: 15, salary: 5, id: 2},
-    {name: 'Danil Buzin', days: 1, salary: 10, id: 3},
-  ]
-  // этот массив должен подтягиваться с сервера со своими key ключами
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a great day', important: false, id: 3 },
-  ]
+  state = {
+    // этот массив должен подтягиваться с сервера со своими key ключами
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a great day', important: false, id: 3 },
+    ],
+    tableData: [
+      {name: 'Nikita Vakula', days: 6, salary: 30, id: 1},
+      {name: 'Danil Zakharov', days: 15, salary: 5, id: 2},
+      {name: 'Danil Buzin', days: 1, salary: 10, id: 3},
+    ]
+  }
 
-  return (
-    <>
-      <div className="table-app">
-        <TableHeader />
-        <TableList props={tableData} />
-        <TableFooter />
-      </div>
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      // формируется новый массив, чтобы не изменять при 
+      // помощи splice старый - так делать в react нельзя
+      const newArray = [
+        ...todoData.slice(0, idx),
+        ...todoData.slice(idx + 1)
+      ];
 
-      <div className="todo-app">
-        <TodoHeader toDo={1} done={3} />
-        <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
+      return {
+        todoData: newArray
+      }
+      
+    });
+  };
+
+  render () {
+    return (
+      <>
+        <div className="table-app d-none">
+          <TableHeader />
+          <TableList props={this.state.tableData} />
+          <TableFooter />
         </div>
 
-        <TodoList todos={todoData} />
-      </div>
-    </>
-  );
-}
+        <div className="todo-app">
+          <TodoHeader toDo={1} done={3} />
+          <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+          </div>
 
-export default App;
+          <TodoList 
+            todos={this.state.todoData} 
+            onDeleted={ this.deleteItem }
+            />
+        </div>
+      </>
+    )
+  }
+}
